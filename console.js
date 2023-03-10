1805cbf5c2b6fff00719ce7fa5ae5c4ebda601cf
mamu0228 <mamu0228@naver.com>
2023-03-10 17:03:02 +0900

    230310

--- console.js
+++ console.js
@@ -1,3 +1,18 @@
+
+/***************************
+이 코드 실행 메뉴얼
+
+1. 먼저 UTM에 로그인을 합니다.
+2. UTM 로그인 후, 룰을 적용해 거부 패킷을 보여주는 화면으로 이동합니다.
+3. console을 키고 '#i)' 코드를 실행해 indexedDB 생성 코드를 실행합니다, 만약 기존 indexedDB가 존재시 초기화 후 다시 실행합니다.
+4. 이후 페이지 맨 밑에 추가된 '파일선택'을 통해 노래를 업로드 해 indexedDB에 노래를 저장합니다.
+5. '#ii)' 코드를 실행해 10초마다 공격 탐지 프로세스를 실행합니다.
+6. 만약 공격이 탐지 돼 노래가 재생될 경우, 그냥 맨 밑에 생긴 Audio 객체의 멈춤 버튼을 누르면 됩니다.
+
+****************************/
+
+// #i) /////////////////////////// 
+
 // indexedDB 생성
 const DB_NAME = 'music_db';
 const DB_VERSION = 1;
@@ -94,6 +109,8 @@ function playMusicFromDB(id) {
 
 /////////// 위 코드들을 먼저 실행해 노래 파일을 업로드 후 밑에 코드 실행
 
+// #ii) ///////////////////////////
+
 // 요청할 URL
 let URL = 'https://example.com';
 let attackArray = ['value1', 'value2', 'value3'];
@@ -103,11 +120,17 @@ let attackArray = ['value1', 'value2', 'value3'];
 function getDataFromUrl() {
   let sipArray = [];
 
+  // URL로 비동기 요청 전송
   fetch(URL)
+    // 정상적으로 응답을 받으면, 그 응답을 response란 변수로 받은 후 text()로 바꿈
     .then(response => response.text())
+    // 이후 얻은 response.text를 data란 매개 변수로 받고 
     .then(data => {
+      // DOMParser()란 객체를 만들고
       const parser = new DOMParser();
+      // response.text를 /text/html로 바꾼다
       const htmlDoc = parser.parseFromString(data, 'text/html');
+      // 만든 thmlDoc이란 변수에서 '.foo'란 클래스 내 있는 데이터를 가져와 elemets에 저장한다
       const elements = htmlDoc.querySelectorAll('.foo');
 
       elements.forEach(element => {
@@ -123,6 +146,7 @@ function getDataFromUrl() {
     .catch(error => {
       console.error('Error:', error);
     });
+    // 배열 내 중복 값을 제거 하기 위해 집합으로 변경하고, 다시 배열로 바꿈
 }
 
 setInterval(getDataFromUrl, 10000);
