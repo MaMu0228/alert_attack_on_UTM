dbb0515444d9d21a35e43d0d6b22c3020f12bf2e
mamu0228 <mamu0228@naver.com>
2023-03-22 16:34:03 +0900

    20230222

--- console.js
+++ console.js
@@ -95,6 +95,60 @@ function playMusicFromDB(id) {
   // request라는 변수에 오브젝트를 불러오는 코드가 성공해 onsuccess 이벤트 발생시 실행
   request.onsuccess = function(event) {
     const musicBlob = event.target.result.music;
+    const audioURL = window.URL.createObjectURL(musicBlob);
+    // window.URL.createObjectURL()도 지원 안 하는 브라우저일 경우 밑 코드 사용
+    // const audioURL = window.webkitURL.createObjectURL(musicBlob);
+
+    var audio = new Audio(audioURL);
+    audio.controls = true;
+    audio.play(); // 음악 파일 재생
+
+    document.body.appendChild(audio);
+
+    console.log('음악 파일 재생');
+  };
+};  
+
+// 쿠키 만드는 초 단위로 만드는 함수
+function setCookie(cookie_name, cookie_value, seconds) {
+  var d = new Date();
+  d.setTime(d.getTime() + (seconds * 1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cookie_name + "=" + cookie_value + ";" + expires + ";path=/";
+}
+
+// 이름으로 받은 쿠기가 있는지 확인하는 함수
+function checkCookie(name) {
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    while (cookie.charAt(0) == " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) == 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+let audio; // audio 변수를 함수 외부에서 선언
+
+// 음악재생
+function playMusicFromDB(id) {
+  const transaction = db.transaction([DB_STORE_NAME], 'readonly');
+  const objectStore = transaction.objectStore(DB_STORE_NAME);
+  // id 인수에 담김 숫자로 오브젝트 스토어에서 데이터를 가져와 request에 넣음
+  const request = objectStore.get(id);
+
+  // request라는 변수에 오브젝트를 불러오는 코드가 실패해 onerror 이벤트 발생시 실행
+  request.onerror = function(event) {
+    console.error('음악 파일 불러오기 오류:', event.target.errorCode);
+  };
+  // request라는 변수에 오브젝트를 불러오는 코드가 성공해 onsuccess 이벤트 발생시 실행
+  request.onsuccess = function(event) {
+    const musicBlob = event.target.result.music;
     const audioURL = URL.createObjectURL(musicBlob);
 
     var audio = new Audio(audioURL);
@@ -111,9 +165,10 @@ function playMusicFromDB(id) {
 
 // #ii) ///////////////////////////
 
-// 요청할 URL
-let URL = 'https://example.com';
-let attackArray = ['value1', 'value2', 'value3'];
+// 요청할 URL, 네이버는 테스트용
+let URL = 'https://www.naver.com/';
+// 테스트용 예시
+let attackArray = ['환율', 'value2', 'value3', 'etc...'];
 
 // URL에서 비동기적으로 요청을 보내 받은 데이터에서 특정 클래스의 데이터를 가져오고
 // 배열들을 비교해, 일치하는 게 있을 시 노래를 재생하는 코드
@@ -131,18 +186,26 @@ function getDataFromUrl() {
       // response.text를 /text/html로 바꾼다
       const htmlDoc = parser.parseFromString(data, 'text/html');
       // 만든 thmlDoc이란 변수에서 '.foo'란 클래스 내 있는 데이터를 가져와 elemets에 저장한다
-      const elements = htmlDoc.querySelectorAll('.foo');
-
+      const elements = htmlDoc.querySelectorAll('.stock_title');
+      
       elements.forEach(element => {
         sipArray.push(element.textContent.trim());
       });
 
-      attackArray.some(value => {
-        if (sipArray.includes(value)) {
-            playMusicFromDB(1);
-            return true;
-        }
-    });
+      const matchingValue = sipArray.find(value => attackArray.includes(value));
+      
+      if (typeof matchingValue !== 'undefined' && !checkCookie("attack_" + matchingValue)){
+        playMusicFromDB(1);
+        setCookie("attack_" + matchingValue, matchingValue, 20);
+      }
+    /*
+      if (sipArray.some(value => attackArray.includes(value))) {
+        playMusicFromDB(1);
+        setCookie("attack_" + value, value, 20);
+      }
+    })
+    */
+    })
     .catch(error => {
       console.error('Error:', error);
     });
